2be301e96cec15cef7afa12c51a2b0dcfda67c44
bairamovazat <bairamovazat@gmail.com>
2019-11-04 16:02:03 +0300

    bf

--- src/main/java/ru/azat/lessons/control/Polynomial.java
+++ src/main/java/ru/azat/lessons/control/Polynomial.java
@@ -79,8 +79,8 @@ public class Polynomial {
 
         for (int i = 0; i < getLength(); i++) {
                 double newValue = isSum ?
-                        this.getCoefficient(i) + this.getCoefficient(i) :
-                        this.getCoefficient(i) - this.getCoefficient(i);
+                        this.getCoefficient(i) + number :
+                        this.getCoefficient(i) - number;
                 result.setCoefficient(i, newValue);
         }
         return result;
@@ -116,8 +116,54 @@ public class Polynomial {
         return result;
     }
 
+    public Polynomial multiple(double number){
+        Polynomial result = new Polynomial(getLength());
+        for (int i = 0; i <= this.getLastIndex(); i++){
+            result.setCoefficient(i, this.getCoefficient(i) * number);
+        }
+        return result;
+    }
+
+    public Polynomial division(double number){
+        Polynomial result = new Polynomial(getLength());
+        for (int i = 0; i <= this.getLastIndex(); i++){
+            result.setCoefficient(i, this.getCoefficient(i) / number);
+        }
+        return result;
+    }
+
     @Override
     public String toString() {
         return Arrays.toString(coefficients);
+    }
+
+    public static void main(String[] args) {
+        Polynomial polynomial1 = new Polynomial(1, 1, 1, 1);
+
+        Polynomial polynomial2 = new Polynomial(1, 1, 1, 1);
+
+        double x = 2.3;
+        System.out.println("Первый полином: ");
+        System.out.println(polynomial1);
+        System.out.println("Второй полином: ");
+        System.out.println(polynomial2);
+        System.out.println("Значение при х = " + x);
+        System.out.println(polynomial1.getFunctionResult(x));
+
+        System.out.println("Сумма первого полинома и второго: ");
+        System.out.println(polynomial1.sum(polynomial2));
+        System.out.println("Разность первого полинома и второго: ");
+        System.out.println(polynomial1.minus(polynomial2));
+        System.out.println("Произведение первого полинома и второго: ");
+        System.out.println(polynomial1.multiple(polynomial2));
+
+        System.out.println("Сумма первого полинома и числа x = " + x);
+        System.out.println(polynomial1.sum(x));
+        System.out.println("Разность первого полинома и числа x = " + x);
+        System.out.println(polynomial1.minus(x));
+        System.out.println("Произведение первого полинома и числа x = " + x);
+        System.out.println(polynomial1.multiple(x));
+        System.out.println("Деление первого полинома и числа x = " + x);
+        System.out.println(polynomial1.division(x));
     }
 }
